d9d7444c2755970f8c7bc1a1c7dfbee522e9adcb
darkwave3137@gmail.com <moo@Silence.localdomain>
2022-04-08 10:55:25 +0800

    first

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,16 @@ public class LeapYear {
             }
         }
     }
+
+	/**
+	 * @param year to be analyzed
+	 */
+	public static boolean isLeapYear(int year) {
+		if(year % 400 == 0)
+			return true;
+		if(year % 4 == 0 && year % 100 != 0)
+			return true;
+		return false;
+	}
 }
 
