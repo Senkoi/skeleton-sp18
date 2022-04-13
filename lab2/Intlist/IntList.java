65c3453fd4ea6a6bf400b956eb30f6a97a20984f
meowjammy <darkwave3137@gmail.com>
2022-04-13 18:46:41 +0800

    fix bug for test null arguments in dcatenate

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -83,6 +83,8 @@ public class IntList {
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
         IntList T = A;
+        if(T == null)
+            return B;
         while(T.rest != null) {
             T = T.rest;
         }
