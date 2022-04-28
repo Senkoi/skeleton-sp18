13bd57dc28cdabb8513fa0fd5f7f9c77cf14f98e
meowjammy <darkwave3137@gmail.com>
2022-04-28 10:22:44 +0800

    switch to size rather than pointer; fix some bugs

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,69 +1,87 @@
 public class ArrayDeque<T> {
     private T[] arr = (T[]) new Object[8];
     private int prev = 0;
-    private int tail = 0;
+    private int size = 0;
     private double Ratio = 0.25;
     public ArrayDeque() {
         arr = (T[]) new Object[8];
         prev = 0;
-        tail = 0;
+        size = 0;
     }
     public ArrayDeque(ArrayDeque other) {
         prev = other.prev;
-        tail = other.tail;
+        size = other.size;
         arr = (T[]) new Object[other.size()];
         System.arraycopy(other, 0, arr, 0, arr.length);
     }
+    private int getFirstIndex() {
+        return (prev % arr.length + arr.length) % arr.length;
+    }
+    private int getLastIndex() {
+        if(size == 0) return getFirstIndex();
+        return (getFirstIndex() + size - 1) % arr.length;
+    }
     private void resize(int capacity) {
         T[] tmp = (T[]) new Object[capacity];
-        System.arraycopy(arr, 0, tmp, 0, tail);
-        System.arraycopy(arr, prev + arr.length, tmp, prev + capacity, Math.abs(prev));
+        if(size > arr.length - 1 - getFirstIndex()) {
+            System.arraycopy(arr, getFirstIndex(), tmp, 0, Math.abs(prev % arr.length));
+            System.arraycopy(arr, 0, tmp, Math.abs(prev % arr.length), size - Math.abs(prev % arr.length));
+        }
+        else{
+            System.arraycopy(arr, getFirstIndex(), tmp,0, size);
+        }
+        prev = 0;
         arr = tmp;
     }
     public void addFirst(T item) {
-        if(arr.length == size()) {
+        if(arr.length - 1 == size()) {
             resize(arr.length * 2);
         }
-        arr[(--prev + arr.length) % arr.length] = item;
+        prev--;
+        arr[getFirstIndex()] = item;
+        size++;
     }
     public void addLast(T item) {
-        if(arr.length == size()) {
+        if(arr.length - 1 == size()) {
             resize(arr.length * 2);
         }
-        arr[(tail++ + arr.length) % arr.length] = item;
+        arr[getLastIndex()] = item;
+        size++;
     }
     public boolean isEmpty() {
-        return size() == 0;
+        return size == 0;
     }
     public int size() {
-        return Math.abs(prev) + Math.abs(tail);
+        return size;
     }
     public void printDeque() {
-        for(int i = prev; i < tail; i++) {
-            System.out.println(arr[(i + arr.length) % arr.length]);
+        for(int i = 0; i < size; i++) {
+            int tmp = (getFirstIndex() + i) % arr.length;
+            System.out.println(arr[tmp]);
         }
     }
     public T removeFirst() {
-        if(prev == tail)
+        if(size == 0)
             return null;
-        T tmp = arr[(prev + arr.length) % arr.length];
+        T tmp = arr[getFirstIndex()];
         //arr[(prev-- + arr.length) % arr.length] = null;
         prev++;
         if((double)size() / arr.length < Ratio && size() > 16)
             resize(arr.length / 2);
+        size--;
         return tmp;
     }
     public T removeLast() {
-        if(prev == tail)
+        if(size == 0)
             return null;
-        T tmp = arr[(tail + arr.length) % arr.length];
+        T tmp = arr[getLastIndex()];
         //arr[(tail-- + arr.length) % arr.length] = null;
-        tail--;
+        size--;
         if((double)size() / arr.length < Ratio && size() > 16)
             resize(arr.length / 2);
         return tmp;
     }
     public T get(int index) {
-        return arr[(prev + index + arr.length) % arr.length];
+        return arr[(getFirstIndex() + index) % arr.length];
     }
 }
